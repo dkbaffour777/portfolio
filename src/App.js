45352211342453b55753f7b97d77f87724dd06d1
Daniel <dkbaffour777@gmail.com>
2021-07-25 20:16:39 -0700

    add about component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Header from "./components/Header/Header";
+import About from "./components/About/About";
 import Footer from "./components/Footer/Footer";
 
 const App = () => {
@@ -6,23 +7,6 @@ const App = () => {
     <>
       <Header />
       <div className="main-content-wrapper">
-        <section id="about-me">
-          <h2>About</h2>
-          <p>
-            Full Stack Web Developer with experience earning a certificate in full stack web development from the
-            University of Arizona. Skills in HTML, CSS and JavaScript and strengths in problem solving, teamwork,
-            meeting deadlines and creativity. Productively rebranded an agency, which resulted an increase in number
-            of leads. <br />
-            Known as an innovative problem solver passionate about approaching programming challenges at different
-            levels and collaborating with others to create meaningful web applications. Positioned to leverage my
-            skills as part of a fast-paced, quality-driven
-            team to build better experiences on software applications. <br />
-            Passionate about music, piano playing to be specific, which has had a major impact of my life by improving my self-confidence and discipline.
-            I’m excited to leverage my skills in the future as part of a fast-paced, quality-driven team. Reach out
-            to me to chat about our next project <a href="#contact-me">here.</a>
-
-          </p>
-        </section>
         <section id="work">
             <h2>Work</h2>
             <div className="work-cont">
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,9 @@
 import Nav from '../Nav/Nav';
 import profile_pic from '../../assets/images/profile-picture.jpg'
-import header_bImg from '../../assets/images/background-theme.jpg'
 
 const Header =()=> {
     return (
-        <header style={{backgroundImage: `url(${header_bImg})`}}>
+        <header>
             <div class="dark-layer primary-dl"></div>
             <div class="logo-wrapper">
                 <img src={profile_pic} alt="Daniel Baffour"/>
