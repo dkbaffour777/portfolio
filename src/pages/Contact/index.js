c8623dda2bcb23b2c6ef70584d11dd38dd1fd027
Daniel <dkbaffour777@gmail.com>
2021-08-26 13:03:29 -0700

    update contact component styles

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,16 +1,16 @@
-import { FaPhone, FaEnvelope } from 'react-icons/fa';
 
 const Contact = () => {
     return (
         <section className="main-sec">
             <h2 data-testid="h2tag">Contact me</h2>
             <address id="contact-info">
+                <div>Send me an email: </div>
                 <div>
-                    <span><FaEnvelope style={{marginRight: "10px"}}/> Send me an email: </span>
                     <a href="mailto:dkbaffour777career@gmail.com">dkbaffour777career@gmail.com</a>
                 </div>
+                <div>-- OR --</div>
                 <div>
-                    <span><FaPhone style={{marginRight: "10px"}}/> Or a text message: </span>
+                    <span>Text message: </span>
                     <span>(602) 571 9734</span>
                 </div>
             </address>
